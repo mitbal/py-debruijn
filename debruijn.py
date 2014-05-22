0da7b38687b65730a137ccf172a410238414aabd
Muhammad Iqbal Tawakal <mit.iqi@gmail.com>
2014-05-23 00:59:32 +0200

    Added function construct_graph to construct the de bruijn graph from list of read's string

--- debruijn.py
+++ debruijn.py
@@ -1,3 +1,12 @@
+class Node:
+	def __init__(self, lab):
+		self.label = lab
+		self.edge = []
+
+class Edge:
+	def __init__(self, lab):
+		self.label = lab
+
 def read_reads(fname):
 	""" Read short reads in FASTA format. It is assumed that one line in the input file correspond to one read. """
 	f = open(fname, 'r')
@@ -11,8 +20,23 @@ def read_reads(fname):
 
 	return reads
 
-def construct_graph(reads):
-	return 0
+def construct_graph(reads, k):
+	""" Construct de bruijn graph from sets of short reads with k length word"""
+	nodes = dict()
+
+	for read in reads:
+		i = 0
+		while i+k < len(read):
+			v1 = read[i:i+k]
+			v2 = read[i+1:i+k+1]
+			if v1 in nodes.keys():
+				nodes[v1] += [Edge(v2)]
+			else:
+				nodes[v1] = [Edge(v2)]
+			i += 1
+
+	return nodes
+
 
 def output_contigs(g):
 	return 0
@@ -21,6 +45,12 @@ def output_contigs(g):
 # Main script
 fname = 'g200reads.fa'
 reads = read_reads(fname)
-print reads
+# print reads
+
+test = ['bcdefg', 'defghi', 'abcd']
+g = construct_graph(test, 3)
+for k in g.keys():
+	
+	print k, g[k]
 # g = construct_graph(reads)
 # contig = output_contigs(g)
